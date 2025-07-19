cca6ed1219e0fd8794268e0e0931346eab7cd126
Naman Muley <naman.g.muley@gmail.com>
2025-07-19 11:41:48 +0530

    Add run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+./install.sh
+
+echo "[run.sh] Service deployed."
+echo "[run.sh] You may now send API requests to http://localhost:8000/"
+echo "[run.sh] See README.md for instructions on how to verify the app."
